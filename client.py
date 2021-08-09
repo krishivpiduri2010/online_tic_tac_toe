e4d2fe99a25163e7e922083ccbe78a634972d7d6
krishivpiduri2010 <59217701+krishivpiduri2010@users.noreply.github.com>
2021-08-09 14:42:22 -0700

    Initial commit

---
+++ client.py
@@ -0,0 +1,5 @@
+import pygame
+import socket
+class Network:
+    def __init__(self):
+        
---
+++ server.py
@@ -0,0 +1,30 @@
+import _thread
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server = '192.168.0.103'
+port = 5555
+
+s.bind((server, port))
+
+print('[SERVER_STARTED] Listening')
+s.listen()
+client_id=0
+
+
+def threaded_client(client:socket.socket):
+    global client_id
+    client.send(str(client_id).encode())
+    while True:
+        try:
+            pass
+        except:
+            break
+    client.close()
+    client_id-=1
+
+
+while True:
+    client,addr=s.accept()
+    _thread.start_new_thread(threaded_client,(client,))
